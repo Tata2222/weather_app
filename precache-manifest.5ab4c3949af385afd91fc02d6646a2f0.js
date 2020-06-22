10d4fc87a189d041b2bd05d1cd604fed487de1cf
Tata2222 <tataolcha@gmail.com>
2020-06-22 13:06:12 +0300

    Updates

---
+++ precache-manifest.5ab4c3949af385afd91fc02d6646a2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c163f048a45a4ad6d6b864332cdc0200",
+    "url": "/CitiesWeatherPage/index.html"
+  },
+  {
+    "revision": "1895e3df7909bc51aed7",
+    "url": "/CitiesWeatherPage/static/css/main.2981f3fe.chunk.css"
+  },
+  {
+    "revision": "aaa5aa840c9676ee6f64",
+    "url": "/CitiesWeatherPage/static/js/2.a3924d2b.chunk.js"
+  },
+  {
+    "revision": "2edade83857e9864b99cb4ff5cce8637",
+    "url": "/CitiesWeatherPage/static/js/2.a3924d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1895e3df7909bc51aed7",
+    "url": "/CitiesWeatherPage/static/js/main.fc8d93bf.chunk.js"
+  },
+  {
+    "revision": "484f535f8ea79b24233b",
+    "url": "/CitiesWeatherPage/static/js/runtime-main.e032c8ca.js"
+  },
+  {
+    "revision": "4054cd89ccc0bc49fb2bff7d91b491be",
+    "url": "/CitiesWeatherPage/static/media/background.4054cd89.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19dab01938e8517f235497f740a01cc0.js"
+  "/CitiesWeatherPage/precache-manifest.5ab4c3949af385afd91fc02d6646a2f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CitiesWeatherPage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
